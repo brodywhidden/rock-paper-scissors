72a175ac29821fb7f63303b7a25445221600c5ee
brodywhidden <160041818+brodywhidden@users.noreply.github.com>
2024-02-26 13:16:50 -0500

    added playRound function

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+const selection = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice() {
-    const selection = ['Rock', 'Paper', 'Scissors'];
-    const computerSelection = selection[Math.floor(Math.random() * selection.length)];
-    return computerSelection
+    const computer = selection[Math.floor(Math.random() * selection.length)];
+    return computer
+}
+
+function playRound (playerSelection) {
+    const player = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
+    const computer = getComputerChoice()
+    if (player == 'Rock' && computer == 'Scissors') {
+        return("You win! Rock beats Scissors!");
+    }
+    if (player == 'Rock'  && computer == 'Paper') {
+        return("You lose! Paper beats Rock!");
+    }
+    if (player == 'Rock'  && computer == 'Rock') {
+        return("It's a draw! Rock vs. Rock");
+    }
+    if (player == 'Paper' && computer == 'Scissors') {
+        return("You lose! Scissors beats Paper!");
+    }
+    if (player == 'Paper'  && computer == 'Paper') {
+        return("It's a draw! Paper vs. Paper");
+    }
+    if (player == 'Paper'  && computer == 'Rock') {
+        return("You win! Paper beats Rock!");
+    }
+    if (player == 'Scissors' && computer == 'Scissors') {
+        return("It's a draw! Scissors vs. Scissors");
+    }
+    if (player == 'Scissors'  && computer == 'Paper') {
+        return("You win! Scissors beats Paper!");
+    }
+    if (player == 'Scissors'  && computer == 'Rock') {
+        return("You lose! Rock beats Scissors!");
+    }
 }
