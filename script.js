6272778354843e05dd75d30157d197a7c66648ef
brodywhidden <160041818+brodywhidden@users.noreply.github.com>
2024-02-26 13:35:38 -0500

    added a one-round playGame function

--- script.js
+++ script.js
@@ -5,34 +5,39 @@ function getComputerChoice() {
     return computer
 }
 
-function playRound (playerSelection) {
+function playRound(playerSelection) {
     const player = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
     const computer = getComputerChoice()
     if (player == 'Rock' && computer == 'Scissors') {
         return("You win! Rock beats Scissors!");
-    }
+        }
     if (player == 'Rock'  && computer == 'Paper') {
         return("You lose! Paper beats Rock!");
-    }
+        }
     if (player == 'Rock'  && computer == 'Rock') {
         return("It's a draw! Rock vs. Rock");
-    }
+        }
     if (player == 'Paper' && computer == 'Scissors') {
         return("You lose! Scissors beats Paper!");
-    }
+        }
     if (player == 'Paper'  && computer == 'Paper') {
         return("It's a draw! Paper vs. Paper");
-    }
+        }
     if (player == 'Paper'  && computer == 'Rock') {
         return("You win! Paper beats Rock!");
-    }
+        }
     if (player == 'Scissors' && computer == 'Scissors') {
         return("It's a draw! Scissors vs. Scissors");
-    }
+        }
     if (player == 'Scissors'  && computer == 'Paper') {
         return("You win! Scissors beats Paper!");
-    }
+        }
     if (player == 'Scissors'  && computer == 'Rock') {
         return("You lose! Rock beats Scissors!");
+        }
     }
-}
+
+    function playGame() {
+        const Game = prompt("Rock, Paper, Scissors!");
+        console.log(playRound(Game))
+    }
