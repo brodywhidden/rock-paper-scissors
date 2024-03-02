c24782de880ae4c2e888a933a7422448c0e38d65
brodywhidden <160041818+brodywhidden@users.noreply.github.com>
2024-03-02 11:20:51 -0500

    Clean up playRound using more efficient if else statements

--- script.js
+++ script.js
@@ -7,35 +7,22 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
-    const player = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
     const computer = getComputerChoice()
-    if (player == 'Rock' && computer == 'Scissors') {
-        return("You win! Rock beats Scissors!");
-        }
-    if (player == 'Rock'  && computer == 'Paper') {
-        return("You lose! Paper beats Rock!");
-        }
-    if (player == 'Rock'  && computer == 'Rock') {
-        return("It's a draw! Rock vs. Rock");
-        }
-    if (player == 'Paper' && computer == 'Scissors') {
-        return("You lose! Scissors beats Paper!");
-        }
-    if (player == 'Paper'  && computer == 'Paper') {
-        return("It's a draw! Paper vs. Paper");
-        }
-    if (player == 'Paper'  && computer == 'Rock') {
-        return("You win! Paper beats Rock!");
-        }
-    if (player == 'Scissors' && computer == 'Scissors') {
-        return("It's a draw! Scissors vs. Scissors");
-        }
-    if (player == 'Scissors'  && computer == 'Paper') {
-        return("You win! Scissors beats Paper!");
-        }
-    if (player == 'Scissors'  && computer == 'Rock') {
-        return("You lose! Rock beats Scissors!");
-        }
-    else
-        return("Invalid input! please select Rock, Paper, or Scissors (case-insensitive)")
+    let resultText;
+
+    if (playerSelection === computer) {
+        resultText = `It's a draw! Both chose ${playerSelection}!`;
+    } else if (
+        (playerSelection == 'Rock' && computer == 'Scissors') ||
+        (playerSelection == 'Paper' && computer == 'Rock') ||
+        (playerSelection == 'Scissors' && computer == 'Paper')
+    ) {
+        resultText = `You win! ${playerSelection} beats ${computer}!`;
+        playerScore++;
+    } else {
+        resultText = `You lose! ${computer} beats ${playerSelection}!`;
+        computerScore++;
     }
+
+    return resultText;
+}
